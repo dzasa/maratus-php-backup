2d695b5fdcf917a693b4d44796f13cc8c8f9ec73
Dzasa <naucnik@gmail.com>
2015-02-09 00:29:29 +0100

    Couchdb use SCP Cuouchdb backup all databases

--- src/Databases/Couchdb.php
+++ src/Databases/Couchdb.php
@@ -2,181 +2,210 @@
 
 namespace Dzasa\MaratusPhpBackup\Databases;
 
-use Symfony\Component\Process\Process;
-use Net_SFTP;
+use Crypt_RSA;
+use Net_SCP;
 use Net_SSH2;
+use Symfony\Component\Process\Process;
 
 class Couchdb {
 
-    private $host = "localhost";
-    private $port = "22";
-    private $user = "";
-    private $pass = "";
-    private $remote = false;
-    private $databaseDir = null;
-    private $database = "";
-    private $backupPath = "";
-    private $backupFilename = "";
-    private $result = array(
-        'status' => null,
-        'message' => ""
-    );
+	private $host = "localhost";
+	private $port = "22";
+	private $user = "";
+	private $pass = "";
+	private $privateKey = "";
+	private $privateKeyPass = "";
+	private $remote = false;
+	private $databaseDir = null;
+	private $database = "";
+	private $backupPath = "";
+	private $backupFilename = "";
+	private $result = array(
+		'status' => null,
+		'message' => "",
+	);
 
-    function __construct($config = array()) {
+	function __construct($config = array()) {
 
-        if (isset($config['remote']) && $config['remote'] == true) {
-            $this->remote = true;
+		if (isset($config['remote']) && $config['remote'] == true) {
+			$this->remote = true;
 
-            if (isset($config['host'])) {
-                $this->host = $config['host'];
-            }
+			if (isset($config['host'])) {
+				$this->host = $config['host'];
+			}
 
-            if (isset($config['port'])) {
-                $this->port = $config['port'];
-            }
+			if (isset($config['port'])) {
+				$this->port = $config['port'];
+			}
 
-            if (isset($config['user'])) {
-                $this->user = $config['user'];
-            }
+			if (isset($config['user'])) {
+				$this->user = $config['user'];
+			}
 
-            if (isset($config['pass'])) {
-                $this->pass = $config['pass'];
-            }
-        }
+			if (isset($config['pass'])) {
+				$this->pass = $config['pass'];
+			}
+		}
 
-        if (isset($config['database'])) {
-            $this->database = $config['database'];
-        }
+		if (isset($config['database'])) {
+			$this->database = $config['database'];
+		}
 
-        $this->backupPath = $config['backup_path'];
+		$this->backupPath = $config['backup_path'];
 
+		if ($this->database == "") {
+			$this->backupFilename = "all-databases-" . date("Y-m-d-H-i-s");
+			$this->backupName = "all-databases-" . date("Y-m-d-H-i-s");
+		} else {
+			$this->backupFilename = $this->database . "-" . date("Y-m-d-H-i-s") . ".couch";
+			$this->backupName = $this->database . "-" . date("Y-m-d-H-i-s");
+		}
+	}
 
-        if ($this->database == "") {
-            $this->backupFilename = "all-databases-" . date("Y-m-d-H-i-s");
-            $this->backupName = "all-databases-" . date("Y-m-d-H-i-s");
-        } else {
-            $this->backupFilename = $this->database . "-" . date("Y-m-d-H-i-s") . ".couch";
-            $this->backupName = $this->database . "-" . date("Y-m-d-H-i-s");
-        }
-    }
+	public function dump() {
+		if (!$this->remote) {
+			$localDumpResult = $this->dumpLocal();
 
-    public function dump() {
-        if (!$this->remote) {
-            $localDumpResult = $this->dumpLocal();
+			if (!$localDumpResult) {
+				return $this->result;
+			}
+		} else {
+			$remoteDumpResult = $this->dumpRemote();
 
-            if (!$localDumpResult) {
-                return $this->result;
-            }
-        } else {
-            $remoteDumpResult = $this->dumpRemote();
+			if (!$remoteDumpResult) {
+				return $this->result;
+			}
+		}
 
-            if (!$remoteDumpResult) {
-                return $this->result;
-            }
-        }
+		$this->result['status'] = 1;
+		$this->result['message'] = "Successful backup of Couchdb in local file: " . $this->backupPath . $this->backupFilename;
+		$this->result['backup_path'] = $this->backupPath;
+		$this->result['backup_filename'] = $this->backupFilename;
+		$this->result['backup_name'] = $this->backupName;
+		$this->result['full_path'] = $this->backupPath . $this->backupFilename;
+		$this->result['host'] = $this->host;
 
+		return $this->result;
+	}
 
-        $this->result['status'] = 1;
-        $this->result['message'] = "Successful backup of Couchdb in local file: " . $this->backupPath . $this->backupFilename;
-        $this->result['backup_path'] = $this->backupPath;
-        $this->result['backup_filename'] = $this->backupFilename;
-        $this->result['backup_name'] = $this->backupName;
-        $this->result['full_path'] = $this->backupPath . $this->backupFilename;
-        $this->result['host'] = $this->host;
+	private function dumpLocal() {
+		$couchDbConfigCommand = 'couch-config --db-dir';
+		$couchDbConfig = new Process($couchDbConfigCommand);
 
+		$couchDbConfig->run();
 
-        return $this->result;
-    }
+		if (!$couchDbConfig->isSuccessful()) {
+			$this->result['status'] = 0;
+			$this->result['message'] = $couchDbConfig->getErrorOutput();
 
-    private function dumpLocal() {
-        $couchDbConfigCommand = 'couch-config --db-dir';
-        $couchDbConfig = new Process($couchDbConfigCommand);
+			return $this->result;
+		}
+		$this->databaseDir = trim($couchDbConfig->getOutput());
+		$this->databaseDir = rtrim($this->databaseDir, "/") . "/";
 
-        $couchDbConfig->run();
+		$filesystem = new \Symfony\Component\Filesystem\Filesystem();
+		if ($this->database != '') {
 
-        if (!$couchDbConfig->isSuccessful()) {
-            $this->result['status'] = 0;
-            $this->result['message'] = $couchDbConfig->getErrorOutput();
+			if (!$filesystem->exists($this->databaseDir . $this->database)) {
+				if (!$filesystem->exists($this->databaseDir . $this->database . ".couch")) {
+					$this->result['status'] = 0;
+					$this->result['message'] = "Defined Couchdb does not exist!";
 
-            return $this->result;
-        }
-        $this->databaseDir = trim($couchDbConfig->getOutput());
-        $this->databaseDir = rtrim($this->databaseDir, "/") . "/";
+					return $this->result;
+				} else {
+					$this->database = $this->database . ".couch";
+				}
+			}
 
-        $filesystem = new \Symfony\Component\Filesystem\Filesystem();
-        if ($this->database != '') {
+			$filesystem->copy($this->databaseDir . $this->database, $this->backupPath . $this->backupFilename);
+		} else {
+			$filesystem->mkdir($this->backupPath . $this->backupFilename);
 
-            if (!$filesystem->exists($this->databaseDir . $this->database)) {
-                if (!$filesystem->exists($this->databaseDir . $this->database . ".couch")) {
-                    $this->result['status'] = 0;
-                    $this->result['message'] = "Defined Couchdb does not exist!";
+			$copyAllDatabasesCommand = 'cp -Ra ' . $this->databaseDir . '. ' . $this->backupPath . $this->backupFilename;
+			$copyAllDatabases = new Process($copyAllDatabasesCommand);
 
-                    return $this->result;
-                } else {
-                    $this->database = $this->database . ".couch";
-                }
-            }
+			$copyAllDatabases->run();
 
-            $filesystem->copy($this->databaseDir . $this->database, $this->backupPath . $this->backupFilename);
-        } else {
-            $filesystem->mkdir($this->backupPath . $this->backupFilename);
+			if (!$copyAllDatabases->isSuccessful()) {
+				$this->result['status'] = 0;
+				$this->result['message'] = $copyAllDatabases->getErrorOutput();
 
-            $copyAllDatabasesCommand = 'cp -Ra ' . $this->databaseDir . '. ' . $this->backupPath . $this->backupFilename;
-            $copyAllDatabases = new Process($copyAllDatabasesCommand);
+				return false;
+			}
+		}
 
-            $copyAllDatabases->run();
+		return true;
+	}
 
-            if (!$copyAllDatabases->isSuccessful()) {
-                $this->result['status'] = 0;
-                $this->result['message'] = $copyAllDatabases->getErrorOutput();
+	private function dumpRemote() {
+		$ssh = new Net_SSH2($this->host, $this->port);
 
-                return false;
-            }
-        }
+		if ($this->privateKey != "") {
+			$key = new Crypt_RSA();
 
-        return true;
-    }
+			if ($this->privateKeyPass != "") {
+				$key->setPassword($this->privateKeyPass);
+			}
 
-    private function dumpRemote() {
-        $ssh = new Net_SSH2($this->host, $this->port);
+			$key->loadKey(file_get_contents($this->privateKey));
 
-        if (!$ssh->login($this->user, $this->pass)) {
-            $this->result['status'] = 0;
-            $this->result['message'] = "Unable to login on SSH!";
+			$login = $ssh->login($this->user, $key);
+		} else {
+			$login = $ssh->login($this->user, $this->pass);
+		}
 
-            return false;
-        }
+		if (!$login) {
+			$this->result['status'] = 0;
+			$this->result['message'] = "Unable to login on SSH!";
 
-        $couchDbConfigCommand = "couch-config --db-dir";
-        //$ssh->enablePTY();
-        $ssh->write($couchDbConfigCommand . "\n");
-        $output = explode("\n", $ssh->read("couchdb"));
+			return false;
+		}
 
+		$couchDbConfigCommand = "couch-config --db-dir";
+		$ssh->write($couchDbConfigCommand . "\n");
+		$output = explode("\n", $ssh->read("couchdb"));
 
-        $databaseDir = end($output);
-        $this->databaseDir = rtrim($databaseDir, "/") . "/";
+		$databaseDir = end($output);
+		$this->databaseDir = rtrim($databaseDir, "/") . "/";
 
+		$scp = new Net_SCP($ssh);
 
-        $sftp = new Net_SFTP($this->host, $this->port);
-        if (!$sftp->login($this->user, $this->pass)) {
-            $this->result['status'] = 0;
-            $this->result['message'] = "Unable to login on SSH!";
+		if ($this->database != '') {
 
-            return false;
-        }
-        if ($this->database != '') {
+			if (!$scp->get($this->databaseDir . $this->database, $this->backupPath . $this->backupFilename)) {
+				if (!$scp->get($this->databaseDir . $this->database . ".couch", $this->backupPath . $this->backupFilename)) {
+					$this->result['status'] = 0;
+					$this->result['message'] = "Unable to copy remote database(s) or wrong database name!";
 
-            if (!$sftp->get($this->databaseDir . $this->database, $this->backupPath . $this->backupFilename)) {
-                if (!$sftp->get($this->databaseDir . $this->database . ".couch", $this->backupPath . $this->backupFilename)) {
-                    $this->result['status'] = 0;
-                    $this->result['message'] = "Unable to copy remote database(s) or wrong database name!";
+					return $this->result;
+				}
+			}
+		} else {
+			$this->backupFilename = rtrim($this->backupFilename, "/") . "/";
+			$couchDbConfigCommand = "ls -a " . $this->databaseDir;
+			$ssh->write($couchDbConfigCommand . "\n");
+			$output = explode("\n", $ssh->exec($couchDbConfigCommand));
+			$filesToDownload = [];
 
-                    return $this->result;
-                }
-            }
-        }
+			foreach ($output as $oValue) {
+				if (strlen($oValue) < 3 || substr($oValue, 0, 1) == "_" || substr($oValue, 0, 1) == '.') {
+					continue;
+				} else {
+					$filesToDownload[] = $oValue;
+				}
+			}
 
-        return true;
-    }
+			if (!is_dir($this->backupPath . $this->backupName)) {
+				mkdir($this->backupPath . $this->backupName);
+			}
+
+			foreach ($filesToDownload as $remoteFile) {
+				$scp->get($this->databaseDir . $remoteFile, $this->backupPath . $this->backupFilename . $remoteFile);
+			}
+
+		}
+
+		return true;
+	}
 
 }
--- src/Databases/Mongodb.php
+++ src/Databases/Mongodb.php
@@ -6,88 +6,84 @@ use Symfony\Component\Process\Process;
 
 class Mongodb {
 
-    private $host = "localhost";
-    private $port = "27017";
-    private $user = "";
-    private $pass = "";
-    private $database = "";
-    private $backupPath = "";
-    private $backupFilename = "";
-    private $result = array(
-        'status' => null,
-        'message' => ""
-    );
+	private $host = "localhost";
+	private $port = "27017";
+	private $user = "";
+	private $pass = "";
+	private $database = "";
+	private $backupPath = "";
+	private $backupFilename = "";
+	private $result = array(
+		'status' => null,
+		'message' => "",
+	);
 
-    function __construct($config = array()) {
-        if (isset($config['host'])) {
-            $this->host = $config['host'];
-        }
+	function __construct($config = array()) {
+		if (isset($config['host'])) {
+			$this->host = $config['host'];
+		}
 
-        if (isset($config['port'])) {
-            $this->port = $config['port'];
-        }
+		if (isset($config['port'])) {
+			$this->port = $config['port'];
+		}
 
-        if (isset($config['user'])) {
-            $this->user = $config['user'];
-        }
+		if (isset($config['user'])) {
+			$this->user = $config['user'];
+		}
 
-        if (isset($config['pass'])) {
-            $this->pass = $config['pass'];
-        }
+		if (isset($config['pass'])) {
+			$this->pass = $config['pass'];
+		}
 
-        if (isset($config['database'])) {
-            $this->database = $config['database'];
-        }
+		if (isset($config['database'])) {
+			$this->database = $config['database'];
+		}
 
-        $this->backupPath = $config['backup_path'];
+		$this->backupPath = $config['backup_path'];
 
+		if ($this->database == "") {
+			$this->backupFilename = "all-databases-" . date("Y-m-d-H-i-s");
+			$this->backupName = "all-databases-" . date("Y-m-d-H-i-s");
+		} else {
+			$this->backupFilename = $this->database . "-" . date("Y-m-d-H-i-s");
+			$this->backupName = $this->database . "-" . date("Y-m-d-H-i-s");
+		}
+	}
 
-        if ($this->database == "") {
-            $this->backupFilename = "all-databases-" . date("Y-m-d-H-i-s");
-            $this->backupName = "all-databases-" . date("Y-m-d-H-i-s");
-        } else {
-            $this->backupFilename = $this->database . "-" . date("Y-m-d-H-i-s");
-            $this->backupName = $this->database . "-" . date("Y-m-d-H-i-s");
-        }
-    }
+	public function dump() {
+		if ($this->user != '' && $this->pass != "") {
+			$this->auth = sprintf("--host='%s' --port='%d' --username='%s' --password='%s'", $this->host, $this->port, $this->user, $this->pass);
+		} else if ($this->user != '' && $this->pass == "") {
+			$this->auth = sprintf("--host='%s' --port='%d' --username='%s'", $this->host, $this->port, $this->user);
+		} else {
+			$this->auth = sprintf("--host='%s' --port='%d'", $this->host, $this->port);
+		}
 
-    public function 
-            dump() {
-        if ($this->user != '' && $this->pass != "") {
-            $this->auth = sprintf("--host='%s' --port='%d' --username='%s' --password='%s'", $this->host, $this->port, $this->user, $this->pass);
-        } else if ($this->user != '' && $this->pass == "") {
-            $this->auth = sprintf("--host='%s' --port='%d' --username='%s'", $this->host, $this->port, $this->user);
-        } else {
-            $this->auth = sprintf("--host='%s' --port='%d'", $this->host, $this->port);
-        }
+		if ($this->database != '') {
+			$command = sprintf('mongodump %s -db %s -out %s', $this->auth, $this->database, $this->backupPath . $this->backupFilename);
+		} else {
+			$command = sprintf('mongodump %s -out %s', $this->auth, $this->backupPath . $this->backupFilename);
+		}
 
-        if ($this->database != '') {
-            $command = sprintf('mongodump %s -db %s -out %s', $this->auth, $this->database, $this->backupPath . $this->backupFilename);
-        } else {
-            $command = sprintf('mongodump %s -out %s', $this->auth, $this->backupPath . $this->backupFilename);
-        }
+		$process = new Process($command);
+		$process->setTimeout(null);
 
+		$process->run();
 
+		if (!$process->isSuccessful()) {
+			$this->result['status'] = 0;
+			$this->result['message'] = $process->getErrorOutput();
+		} else {
+			$this->result['status'] = 1;
+			$this->result['message'] = "Successful backup of Mongodb in local file: " . $this->backupPath . $this->backupFilename;
+			$this->result['backup_path'] = $this->backupPath;
+			$this->result['backup_filename'] = $this->backupFilename;
+			$this->result['backup_name'] = $this->backupName;
+			$this->result['full_path'] = $this->backupPath . $this->backupFilename;
+			$this->result['host'] = $this->host;
+		}
 
-        $process = new Process($command);
-        $process->setTimeout(null);
-
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            $this->result['status'] = 0;
-            $this->result['message'] = $process->getErrorOutput();
-        } else {
-            $this->result['status'] = 1;
-            $this->result['message'] = "Successful backup of Mongodb in local file: " . $this->backupPath . $this->backupFilename;
-            $this->result['backup_path'] = $this->backupPath;
-            $this->result['backup_filename'] = $this->backupFilename;
-            $this->result['backup_name'] = $this->backupName;
-            $this->result['full_path'] = $this->backupPath . $this->backupFilename;
-            $this->result['host'] = $this->host;
-        }
-
-        return $this->result;
-    }
+		return $this->result;
+	}
 
 }
