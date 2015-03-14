3e82db526bee0da193acfd921d777faa31395878
Dzasa <naucnik@gmail.com>
2015-03-14 02:35:02 +0100

    Delete uneeded files

--- google_auth.php
+++ google_auth.php
@@ -3,9 +3,9 @@
 require 'vendor/autoload.php';
 
 $gDriveConfig = array(
-    'client_id' => '',
-    'client_secret' => '',
-    'auth_code' => ""
+	'client_id' => '',
+	'client_secret' => '',
+	'auth_code' => "",
 );
 
 $client = new Google_Client();
@@ -18,7 +18,7 @@ $client->setRedirectUri("urn:ietf:wg:oauth:2.0:oob");
 $client->setScopes(array("https://www.googleapis.com/auth/drive"));
 
 if (!file_exists("token.json")) {
-    // Save token for future use
-    $accessToken = $client->authenticate($gDriveConfig['auth_code']);
-    file_put_contents("token.json", $accessToken);
+	// Save token for future use
+	$accessToken = $client->authenticate($gDriveConfig['auth_code']);
+	file_put_contents("token.json", $accessToken);
 }
